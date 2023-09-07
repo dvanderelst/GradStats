f7e8dfd4e0a46c08978dcacce4653bf82199fab2
Dieter <dieter.vanderelst@fastmail.net>
2023-09-07 09:19:22 -0400

    more edits to this file

--- ClassPrep/differences_of_means_exercise.R
+++ ClassPrep/differences_of_means_exercise.R
@@ -5,7 +5,8 @@ sd_population <- 1.5
 
 
 differences <-c()
-for (i in 1:10000)
+p_values<-c()
+for (i in 1:100000)
 {
   men <- rnorm(sample_size, mean = population_men_mean, sd=sd_population)
   women <- rnorm(sample_size, mean = population_women_mean, sd=sd_population)
@@ -15,7 +16,7 @@ for (i in 1:10000)
   differences <- c(differences, difference)
 }
 
-sampling_distribution_mean<- women_mean - men_mean
+sampling_distribution_mean<- population_women_mean - population_men_mean
 
 sample_sd <- sd_population / sqrt(sample_size)
 sampling_distribution_sd <- sqrt(sample_sd^2 + sample_sd^2)
@@ -25,3 +26,7 @@ y <- dnorm(x, mean =  sampling_distribution_mean, sd = sampling_distribution_sd)
   
 hist(differences, freq=F,breaks=50)
 points(x,y, t='l')
+
+
+simulated_p <- mean(differences > 1.5)
+
